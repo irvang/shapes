2dbb541a8c4dffb781e0840a935d03b224438b31
= <ia.manage@outlookcom>
2021-02-07 09:08:16 -0500

    Saving work

--- Shaper.java
+++ Shaper.java
@@ -1,6 +1,4 @@
 
-import java.util.ArrayList;
-
 import javafx.scene.Group;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Line;
@@ -14,19 +12,45 @@ public class Shaper
     private Line line4;
     private Line line5;
     private Line line6;
-		private Color color;
-		// private Group rectangle;
-		Line [] rectangle;
-		Line [] triangle;
-Line [] hexagon;
+    private Color color;
+    // private Group rectangle;
+    Line[] rectangle = new Line[4];
+    Line[] triangle = new Line[3];
+    Line[] hexagon = new Line[6];
 
     public Shaper() {
 	// initialize mainGroup
 	mainGroup = new Group();
 	// rectangle = new Group();
+	createShapes();
+
 	setHexagon();
 	color = Color.RED;
     }
+
+    private void createShapes() {
+
+	rectangle[0] = new Line(100, 70, 100, 250);
+	rectangle[1] = new Line(100, 250, 400, 250);
+	rectangle[2] = new Line(400, 250, 400, 70);
+	rectangle[3] = new Line(100, 70, 400, 70);
+
+	triangle[0] = new Line(100, 70, 100, 250);
+	triangle[1] = new Line(100, 250, 400, 250);
+	triangle[2] = new Line(400, 250, 100, 70);
+
+	hexagon[0] = new Line(295, 82, 205, 82);
+	hexagon[1] = new Line(205, 82, 160, 160);
+	hexagon[2] = new Line(160, 160, 205, 238);
+	hexagon[3] = new Line(205, 238, 295, 238);
+	hexagon[4] = new Line(295, 238, 340, 160);
+	hexagon[5] = new Line(340, 160, 295, 82);
+
+		}
+		
+		private void resetColor2() {
+// for ()
+		}
 
     public Group getMainGroup() {
 	return mainGroup;
@@ -47,7 +71,7 @@ Line [] hexagon;
 	line2 = new Line(100, 250, 400, 250);
 	line3 = new Line(400, 250, 400, 70);
 	line4 = new Line(100, 70, 400, 70);
-  Line[] rectangle = {line1, line2, line3, line4};
+	Line[] rectangle = { line1, line2, line3, line4 };
 	resetColor();
 	// rectangle.getChildren().addAll(line1, line2, line3, line4);
 	// mainGroup.getChildren().addAll(line1, line2, line3, line4);
